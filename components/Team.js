b2ec6bae34ec6bc36634af13054eee28994b7fa8
Codehash001 <codehashblockchain@gmail.com>
2023-02-12 10:06:51 +0530

    update

--- components/Team.js
+++ components/Team.js
@@ -5,8 +5,8 @@ function Team() {
     <div id='team' className='flex flex-col items-center md:px-16 mt-10 mb-6'>
         <h1 className='font-bold font-Archivo text-5xl my-10 bg-gradient-to-r from-orange-500 to-red-700 bg-clip-text text-transparent' data-aos="fade-up">Team</h1>
 
-        <div className='flex md:flex-row flex-col items-center md:items-start justify-between' data-aos="fade-up" data-aos-delay="100">
-            <div className='flex flex-col items-center p-6'>
+        <div className='flex md:flex-row flex-col items-center md:items-start justify-between'>
+            <div className='flex flex-col items-center p-6'  data-aos="fade-up" data-aos-delay="100">
                 <img src='/owner.png' className='w-[300px] h-[300px]'/>
                 <h1 className='font-Archivo text-2xl font-semibold mt-3 mb-2 text-brand-01'>Mr.Phoenyx</h1>
                 <h1 className='font-Archivo text-xl font-medium text-center'>Cafe Owner, Majestic Battista</h1>
